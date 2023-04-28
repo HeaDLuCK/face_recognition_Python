a61be217bea752ad287a71280b66daa7e7932b53
headluck <106028299+HeaDLuCK@users.noreply.github.com>
2023-04-28 13:04:46 +0100

    test

--- Entry Camera.py
+++ Entry Camera.py
@@ -37,62 +37,66 @@ while True:
     FrameResize = cv2.resize(frame, (0, 0), None, 0.5, 0.5)
     facesLoc = face_recognition.face_locations(FrameResize)
     encodeImgs = face_recognition.face_encodings(FrameResize, facesLoc)
-    fulldate = datetime.now().replace(second=0, microsecond=0)
-    splittedDate = str(fulldate).split(" ")
-    date = splittedDate[0]
-    hour = splittedDate[1]
-    hourplusOne = str(fulldate+timedelta(minutes=1)).split(" ")[1]
-    hourplusTwo = str(fulldate+timedelta(minutes=2)).split(" ")[1]
-    for faceLoc, encodeImg in zip(facesLoc, encodeImgs):
-        results = face_recognition.compare_faces(EncImg, encodeImg, 0.6)
-        face_Dis = face_recognition.face_distance(EncImg, encodeImg)
+    print(facesLoc)
 
-        cv2.rectangle(frame, (faceLoc[3]*2, faceLoc[0]*2),
-                      (faceLoc[1]*2, faceLoc[2]*2), (25, 155, 12), 1)
-        index = np.argmin(face_Dis)
-        if results[index]:
-            perso_id = className[index]
-            dateCheck = collection.find_one({"date": date})
-            if (dateCheck):
-                a = collection.find_one(
-                    {"presence.checkIn": {'$in': [hour, hourplusOne, hourplusTwo]}, "date": date, "presence.perso_id": perso_id})
-                if (a):
-                    pass
-                else:
-                    collection.update_one(
-                        {"_id": dateCheck['_id']},
-                        {"$push": {"presence": {"perso_id": perso_id, "checkIn": hour,
-                                   "checkOut": 'null'}}})
-            else:
+    # fulldate = datetime.now().replace(second=0, microsecond=0)
+    # splittedDate = str(fulldate).split(" ")
+    # date = splittedDate[0]
+    # hour = splittedDate[1]
+    # hourplusOne = str(fulldate+timedelta(minutes=1)).split(" ")[1]
+    # hourplusTwo = str(fulldate+timedelta(minutes=2)).split(" ")[1]
+    # for faceLoc, encodeImg in zip(facesLoc, encodeImgs):
+    #     results = face_recognition.compare_faces(EncImg, encodeImg, 0.6)
+    #     face_Dis = face_recognition.face_distance(EncImg, encodeImg)
 
-                collection.insert_one(
-                    {"date": date, "presence": [
-                        {"perso_id": perso_id, "checkIn": hour, "checkOut": 'null'}
-                    ]})
+    #     cv2.rectangle(frame, (faceLoc[3]*2, faceLoc[0]*2),
+    #                   (faceLoc[1]*2, faceLoc[2]*2), (25, 155, 12), 1)
+    #     index = np.argmin(face_Dis)
+        # if results[index]:
+        #     perso_id = className[index]
+        #     print(perso_id)
+            # dateCheck = collection.find_one({"date": date})
+            # if (dateCheck):
+            #     a = collection.find_one(
+            #         {"presence.checkIn": {'$in': [hour, hourplusOne, hourplusTwo]}, "date": date, "presence.perso_id": perso_id})
+            #     if (a):
+            #         pass
+            #     else:
+            #         collection.update_one(
+            #             {"_id": dateCheck['_id']},
+            #             {"$push": {"presence": {"perso_id": perso_id, "checkIn": hour,
+            #                        "checkOut": 'null'}}})
+            # else:
 
-        else:
-            a = collection.find_one({"date": date})
-            filename = '_'.join((date.split('-'))) + "T" + \
-                '_'.join(hour.split(':'))
-            path = f"./unkown personnes/{filename}.png"
-            cv2.imwrite(path, FrameResize)
-            if (a):
-                check = collection.find_one(
-                    {"presence.checkIn": {'$in': [hour, hourplusOne, hourplusTwo]}, "date": date, "presence.perso_id": filename})
-                if (check):
-                    pass
-                else:
-                    collection.update_one(
-                        {"_id": a['_id']},
-                        {'$push': {
-                            "presence": {"perso_id": filename, "checkIn": hour,
-                                         "checkOut": 'null'}
-                        }})
-            else:
-                collection.insert_one(
-                    {"date": date, "presence": [
-                        {"perso_id": filename, "checkIn": hour, "checkOut": 'null'}
-                    ]})
+            #     collection.insert_one(
+            #         {"date": date, "presence": [
+            #             {"perso_id": perso_id, "checkIn": hour, "checkOut": 'null'}
+            #         ]})
+
+        # else:
+        #     print("Unkown")
+            # a = collection.find_one({"date": date})
+            # filename = '_'.join((date.split('-'))) + "T" + \
+            #     '_'.join(hour.split(':'))
+            # path = f"./unkown personnes/{filename}.png"
+            # cv2.imwrite(path, FrameResize)
+            # if (a):
+            #     check = collection.find_one(
+            #         {"presence.checkIn": {'$in': [hour, hourplusOne, hourplusTwo]}, "date": date, "presence.perso_id": filename})
+            #     if (check):
+            #         pass
+            #     else:
+            #         collection.update_one(
+            #             {"_id": a['_id']},
+            #             {'$push': {
+            #                 "presence": {"perso_id": filename, "checkIn": hour,
+            #                              "checkOut": 'null'}
+            #             }})
+            # else:
+            #     collection.insert_one(
+            #         {"date": date, "presence": [
+            #             {"perso_id": filename, "checkIn": hour, "checkOut": 'null'}
+            #         ]})
 
     cv2.imshow("test", frame)
     if cv2.waitKey(1) == ord("x"):
